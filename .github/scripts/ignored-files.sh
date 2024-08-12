7e9603878cd76daf0bb9895b3529270dc8106df8
MrKaplan <169855803+MrKaplan-lw@users.noreply.github.com>
2024-08-13 01:25:36 +0200

    test

--- .github/scripts/ignored-files.sh
+++ .github/scripts/ignored-files.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -euxo pipefail
 apk add git
+# GHA workspace file ownership is "dubious"
+git config --global --add safe.directory /github/workspace
 IGNORED=$(git ls-files --cached -i --exclude-standard)
 if [[ "$IGNORED" ]]; then echo "Ignored files present:\n$IGNORED\n"; exit 1; fi
